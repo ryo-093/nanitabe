eb4cdc7655532bde00cda0a3f59da87d3d91f323
ryo-093 <o.ryohei.wk@gmail.com>
2023-03-10 21:08:01 +0900

    kari

---
+++ src/js/api.js
@@ -0,0 +1,56 @@
+const { normalize } = require('@geolonia/normalize-japanese-addresses')
+
+export const getLatLng = (str, callback, errorCallback = () => {}) => {
+  try {
+    normalize(str).then(json => {
+      if (json.level > 0) {
+        callback(json)
+      } else {
+        const e = new Error('見つかりませんでした。住所を修正して、もう一度お試しください!')
+        errorCallback(e)
+      }
+    }).catch(() => {
+      const e = new Error('見つかりませんでした。住所を修正して、もう一度お試しください。')
+      errorCallback(e)
+    })
+  } catch (e) {
+    errorCallback(e)
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.getLatLng = getLatLng
+}
+
+export const getLatLngArray = async(arrs, callback, errorCallback = () => {}) => {
+    let reArr = [];
+
+    for (let i = 0; i < arrs.length; i++){
+        try {
+            await normalize(arrs[i].PickupPlace).then(json => {
+              if (json.level > 0) {
+                //   console.log(json)
+                console.log(i)
+                reArr.push(json)
+                // callback(reArr)
+                if (i === (arrs.length - 1)){
+                    console.log(reArr)
+                    callback(reArr)
+                }
+              } else {
+                const e = new Error('見つかりませんでした。住所を修正して、もう一度お試しください!')
+                errorCallback(e)
+              }
+            }).catch(() => {
+              const e = new Error('見つかりませんでした。住所を修正して、もう一度お試しください。')
+              errorCallback(e)
+            })
+          } catch (e) {
+            errorCallback(e)
+          }
+    }
+}
+  
+  if (typeof window !== 'undefined') {
+    window.getLatLngArray = getLatLngArray
+  }
--- src/js/mapArea.js
+++ src/js/mapArea.js
@@ -1,21 +1,22 @@
-import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from 'leaflet';
 
 import { useSearchParams } from "react-router-dom";
 import { DataStore } from '@aws-amplify/datastore';
 import { UserTable } from './../models';
+import { SellTable } from './../models';
 
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-import {getLatLng} from "https://cdn.geolonia.com/community-geocoder.js";
+import { getLatLng , getLatLngArray} from "./../js/api.js";
 
-function defCustomMarker(imageUrl){
+function defCustomMarker(imageUrl) {
     const customMarker = new L.Icon({
         shadowUrl: null,
         iconUrl: imageUrl,
         iconRetinaUrl: imageUrl,
-        popupAnchor:  [-0, -0],
-        iconSize: [45,45],
+        popupAnchor: [-0, -0],
+        iconSize: [45, 45],
         className: "icon",
     });
     return customMarker;
@@ -26,30 +27,76 @@ const MapArea = () => {
     const getNickname = searchParams.get("name");
 
     const [list, setList] = useState([]);
-    const [userAddress, setUserAddress] = useState();
+    const [goods, setGoods] = useState([]);
+    const [position, setPosition] = useState([35.689634, 139.692101]);
+    // const [gPosition, setGPosition] = useState({"lat": 35.689634, "lng": 139.692101});
+    const [gPosition, setGPosition] = useState([]);
 
-    const fetchData = async() => {
-        const res = (await DataStore.query(UserTable)).filter(c => c.nickname === getNickname);
-        // const res = (await DataStore.query(UserTable));
-        setList(res[0]);
-        getLatLng(res[0].currentAddress, setUserAddress(res[0].currentAddress));
-        console.log("getting user info. success")
-    };
+    // const fetchData = async () => {
+    //     const res = (await DataStore.query(UserTable)).filter(c => c.nickname === getNickname);
+    //     setList(res[0]);
 
-    useEffect( ()=> {
-        fetchData()
+    //     getLatLng(res[0].currentAddress, (latlng) => {
+    //         setPosition([latlng.lat, latlng.lng])
+    //         console.log("getting user info. success")
+    //     });
+    // };
+
+    // const fetchGdata = async () => {
+    //     console.log('getting position info started')
+    //     const res = await DataStore.query(SellTable);
+    //     setGoods(res);
+
+    //     // console.log(res)
+
+    //     getLatLngArray(res, (latlng) => {
+    //         setGPosition(latlng)
+    //         console.log("getting position info. success")
+    //         },
+    //         (re) => console.log(re)
+    //         )
+    // };
+
+    // useEffect(() => {
+    //     fetchGdata()    //商品情報
+    //     fetchData()     //ユーザー情報
+    // }, []);
+
+    useEffect(() => {
+        // fetchGdata()    //商品情報
+        const fetchData = async () => {
+            const res1 = (await DataStore.query(UserTable)).filter(c => c.nickname === getNickname);
+            setList(res1[0]);
+    
+            console.log('ユーザー情報取得開始')
+            getLatLng(res1[0].currentAddress, (latlng) => {
+                setPosition([latlng.lat, latlng.lng])
+                console.log("getting user info. success")
+            });
+    
+            console.log('getting position info started')
+            const res = await DataStore.query(SellTable);
+            setGoods(res);
+    
+            // console.log(res)  
+    
+            getLatLngArray(res, (latlng) => {
+                setGPosition(latlng)
+                // console.log(latlng)
+                // console.log(gPosition)
+                // console.log(latlng)
+                console.log("getting position info. success")
+                },
+                (re) => console.log(re)
+                )
+        };
+
+        fetchData()     //ユーザー情報
+        console.log('実行されました')
     }, []);
 
-    console.log(userAddress)
-    // if (list.currentAddress) {
-        // getLatLng(document.getElementById('address').value, (latlng) => {
-        //   map.setCenter(latlng)
-        // })
-    // }
-    // とりあえず都庁
-    const position = [35.689634, 139.692101]; 
-    //テスト用
-    const datas = require('./testForPositions.json')
+    //テスト
+    // const goods = require('./testForPositions.json')
 
     const MapContainerStyle = {
         width: "100%",
@@ -57,6 +104,9 @@ const MapArea = () => {
         // display: "inline-block",
         // padding: "5px"
     };
+
+    // console.log(goods[0])
+    // console.log({gPosition})
 
     return (
         <MapContainer center={position} zoom={16} scrollWheelZoom={false} style={MapContainerStyle}>
@@ -69,14 +119,15 @@ const MapArea = () => {
                     You're here!
                 </Popup>
             </Marker>
-            {datas.map((data) => (
-					<Marker key={data.id} position={[ data.position[0], data.position[1] ]} icon={defCustomMarker(data.imageUrl)}>
-						<Popup position={[ data.position[0], data.position[1] ]}>
+            {goods.map((good, index) => (
+					<Marker key={good.id} position={[ gPosition[index].lat, gPosition[index].lng ]} icon={defCustomMarker(good.imageUrl)}>
+						<Popup position={[ gPosition[index].lat, gPosition[index].lng ]}>
+                    {/* // <Marker key={good.id} position={[35.689634, 139.692101]} icon={defCustomMarker(good.imageUrl)}>
+					// 	<Popup position={[35.689634, 139.692101]}> */}
 							<div>
-								<h3>{data.food}</h3>
-								<h4>金額：{data.amount}円</h4>
-                                <p>{data.comment}</p>
-                                <a href="./timeline">詳細を見る</a>
+								<h3>{good.goodsName}</h3>
+                                <p>{good.comment}</p>
+                                <a href="./detail">詳細を見る</a>
 							</div>
 						</Popup>
 					</Marker>
